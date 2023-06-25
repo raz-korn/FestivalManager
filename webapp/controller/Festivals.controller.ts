3ac909420007a4d7a93555667e9d82e455bc79c6
Raz Korn <raz.korn@sap.com>
2023-06-25 09:33:40 +0000

    Added fragment with category filtering

--- webapp/controller/Festivals.controller.ts
+++ webapp/controller/Festivals.controller.ts
@@ -7,6 +7,10 @@ import { IconColor } from "sap/ui/core/library";
 import ListBinding from "sap/ui/model/ListBinding";
 import Filter from "sap/ui/model/Filter";
 import Event from "sap/ui/base/Event";
+import SelectDialog from "sap/m/SelectDialog";
+import Control from "sap/ui/core/Control";
+import Fragment from "sap/ui/core/Fragment";
+import StandardListItem from "sap/m/StandardListItem";
 
 enum Threshold {
     Moderate = 50,
@@ -66,7 +70,51 @@ export default class Festivals extends Controller {
             this.statusFilters = [];
         }
 
-        listBinding.filter(this.statusFilters);
+        listBinding.filter(this.statusFilters.concat(this.customFilters));
     }
 
+    private dialog: Promise<SelectDialog>;
+
+    onCategorySelect(): void{
+        if(!(this.dialog instanceof Promise)) {
+
+            const festivalModel = this.getFestivalModel();
+            const resourceModel = this.getView()?.getModel("i18n") as ResourceModel;
+
+            this.dialog = Fragment.load({
+                type: "XML",
+                name: "keepdancing.festivalmanager.view.CategorySelectDialog",
+                controller: this
+            }).then(function(control: Control|Control[]){
+                const dialog = (control instanceof Array ? control[0] : control) as SelectDialog;
+                dialog.setModel(festivalModel, "festivalModel");
+                dialog.setModel(resourceModel, "i18n");
+                dialog.setMultiSelect(true);
+                return dialog;
+            });
+        }
+
+        this.dialog.then(function(dialog){
+            dialog.open("");
+        }).catch(function(err: Error){
+            MessageToast.show(err.message);
+        });
+    }
+    
+    onCategorySelectChange(event: Event): void {
+        const value = (event.getParameter("value") as string);
+        const filter = new Filter("name", "Contains", value);
+        const listBinding = (event.getSource() as Control).getBinding("items") as ListBinding;
+        listBinding.filter([filter]);
+    }
+
+    onCategorySelectConfirm(event: Event): void {
+        const selectedItems = (event.getParameter("selectedItems") as StandardListItem[]);
+        const listBinding = this.getView()?.byId("festivalsList")?.getBinding("items") as ListBinding;
+        this.customFilters = selectedItems.map(function(item: StandardListItem) {
+            return new Filter("category", "EQ", item.getTitle());
+        });
+        listBinding.filter(this.customFilters.concat(this.statusFilters));
+    }
+    
 }
